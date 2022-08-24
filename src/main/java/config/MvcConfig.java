b2901b895b911a680c6c7ebdc39c41469e2ae68c
eeun <eeun95@gmail.com>
2022-08-24 18:32:54 +0900

    MVC 1: 요청 매핑, 커맨드 객체, 리다이렉트, 폼 태그, 모델

--- src/main/java/config/MvcConfig.java
+++ src/main/java/config/MvcConfig.java
@@ -11,13 +11,14 @@ import org.springframework.web.servlet.config.annotation.*;
 public class MvcConfig implements WebMvcConfigurer {
 
     // DispatcherServlet의 매핑 경로를 '/'로 주었을 때, JSP/HTML/CSS 등을 올바르게 처리하기 위한 설정을 추가한다.
-    /*
+    // SpringBoot에서는 사실상 DispatcherServlet만 사용하기 떄문에 기본적으로 등록되던 DefaultServlet이 더 이상 등록되지 않는다
+    // 그래서 필요시 프로퍼티 파일에 server.servlet.register-default-servlet=true 추가해줘야함
     @Override
     public void configureDefaultServletHandling(DefaultServletHandlerConfigurer configurer) {
         configurer.enable();
     }
+
     // JSP를 이용해서 컨트롤러의 실행 결과를 보여주기 위한 설정을 추가한다.
-    */
     @Override
     public void configureViewResolvers(ViewResolverRegistry registry) {
         registry.jsp("/WEB-INF/view/", ".jsp");
