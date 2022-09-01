419d77ed9c3534e111d8e3548c34fa1f1c1b534c
eeun <eeun95@gmail.com>
2022-09-01 22:13:00 +0900

    MVC 3: 세션, 인터셉터, 쿠키

--- src/main/java/controller/LoginCommandValidator.java
+++ src/main/java/controller/LoginCommandValidator.java
@@ -1,4 +1,21 @@
 package controller;
 
-public class LoginCommandValidator {
+import org.springframework.validation.Errors;
+import org.springframework.validation.ValidationUtils;
+import org.springframework.validation.Validator;
+
+public class LoginCommandValidator implements Validator {
+
+    @Override
+    public boolean supports(Class<?> clazz) {
+        // LoginCommand 클래스로 타입 변환이 가능한지 확인
+        return LoginCommand.class.isAssignableFrom(clazz);
+    }
+
+    @Override
+    public void validate(Object target, Errors errors) {
+        ValidationUtils.rejectIfEmptyOrWhitespace(errors, "email", "required");
+        ValidationUtils.rejectIfEmpty(errors, "password", "required");
+
+    }
 }
