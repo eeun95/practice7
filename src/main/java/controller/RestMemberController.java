3e28bb5fa9e41daa44231a41965942627e4b8a95
eeun <eeun95@gmail.com>
2022-09-17 18:39:42 +0900

    JSON 응답과 요청 처리

--- src/main/java/controller/RestMemberController.java
+++ src/main/java/controller/RestMemberController.java
@@ -1,14 +1,11 @@
 package controller;
 
 import com.fasterxml.classmate.MemberResolver;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.RestController;
-import spring.Member;
-import spring.MemberDao;
-import spring.MemberRegisterService;
+import org.springframework.web.bind.annotation.*;
+import spring.*;
 
 import javax.servlet.http.HttpServletResponse;
+import javax.validation.Valid;
 import java.io.IOException;
 import java.util.List;
 
@@ -40,4 +37,16 @@ public class RestMemberController {
     public void setMemberRegisterService(MemberRegisterService memberRegisterService) {
         this.memberRegisterService = memberRegisterService;
     }
+
+    @PostMapping("/api/members")
+    public void newMember(@RequestBody @Valid RegisterRequest regReq, HttpServletResponse response) throws IOException {
+        try {
+            Long newMemberId = memberRegisterService.regist(regReq);
+            response.setHeader("Location", "/api/members" + newMemberId);
+            response.setStatus(HttpServletResponse.SC_CREATED);
+        } catch (DuplicateMemberException duplicateMemberException) {
+            response.sendError(HttpServletResponse.SC_CONFLICT);
+        }
+    }
+
 }
